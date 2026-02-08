ef0e38440c0c79c5e74b1f617d74b7f36dd34001
Haji <hacih4451@gmail.com>
2026-02-08 23:44:59 +0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,24 +1,10 @@
-#!/usr/bin/python3
 import sys
 
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1   # Decrease n to avoid infinite loop
+        n -= 1
     return result
 
-if len(sys.argv) != 2:
-    print("Usage: python3 script.py <number>")
-    sys.exit(1)
-
-try:
-    num = int(sys.argv[1])
-    if num < 0:
-        raise ValueError("Number must be non-negative")
-except ValueError as e:
-    print("Error:", e)
-    sys.exit(1)
-
-f = factorial(num)
-print(f)
+print(factorial(int(sys.argv[1])))
